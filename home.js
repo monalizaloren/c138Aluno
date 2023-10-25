b5b80f6df9387116d2f0eb091bd085b9073e3ad4
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-10-25 15:48:20 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,47 +1,128 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Image,
+  ImageBackground,
+} from "react-native";
+import axios from "axios";
+
+export default class DetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: {},
+      imagePath: "",
+      url: `https://2431-2405-201-8008-e095-a413-c282-beb3-6a86.ngrok.io/planet?name=${this.props.navigation.getParam(
+        "planet_name"
+      )}`,
+    };
+  }
+
+  componentDidMount() {
+    this.getDetails();
+  }
+  getDetails = () => {
+    const { url } = this.state;
+    axios
+      .get(url)
+      .then((response) => {
+        this.setDetails(response.data.data);
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
+  };
+
+  setDetails = (planetDetails) => {
+    const planetType = planetDetails.planet_type;
+    let imagePath = "";
+    switch (planetType) {
+      case "Gas Giant":
+        imagePath = require("../assets/Gas_Giant.png");
+        break;
+      case "Terrestrial":
+        imagePath = require("../assets/Terrestrial.png");
+        break;
+      case "Super Earth":
+        imagePath = require("../assets/Super_Earth.png");
+        break;
+      case "Neptune Like":
+        imagePath = require("../assets/Neptune-like.png");
+        break;
+      default:
+        imagePath = require("../assets/Gas_Giant.png");
+    }
+
+    this.setState({
+      details: planetDetails,
+      imagePath: imagePath,
+    });
+  };
 
   render() {
-    const { listData } = this.state;
-
-    if (listData.length != 0) {
+    const { details, imagePath } = this.state;
+    if (details.specifications) {
       return (
         <View style={styles.container}>
-          <SafeAreaView
-            style={{
-              marginTop:
-                Platform.OS === "android" ? StatusBar.currentHeight : 0,
-            }}
-          />
           <ImageBackground
             source={require("../assets/bg.png")}
             style={{ flex: 1, paddingTop: 20 }}
           >
-            <View style={styles.upperContainer}>
-              <Text style={styles.headerText}>Mundo dos Planetas</Text>
-            </View>
-            <View style={styles.lowerContainer}>
-              <FlatList
-                keyExtractor={this.keyExtractor}
-                data={this.state.listData}
-                renderItem={this.renderItem}
-                numColumns={2}
-              />
+            <Image
+              source={imagePath}
+              style={{
+                height: 250,
+                width: 250,
+                marginTop: 50,
+                alignSelf: "center",
+              }}
+            />
+            <View style={{ marginTop: 50 }}>
+              <Text style={styles.planetName}>{details.name}</Text>
+              <View style={{ alignSelf: "center" }}>
+                <Text
+                  style={styles.planetData}
+                >{`Distância da Terra: ${details.distance_from_earth}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Distância do Sol: ${details.distance_from_their_sun}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Gravidade: ${details.gravity}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Período Orbital: ${details.orbital_period}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Velocidade Orbital: ${details.orbital_speed.toFixed(8)}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Massa do Planeta: ${details.planet_mass}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Raio do Planeta: ${details.planet_radius}`}</Text>
+                <Text
+                  style={styles.planetData}
+                >{`Tipo do Planeta: ${details.planet_type}`}</Text>
+                <View style={{ flexDirection: "row",alignSelf:"center" }}>
+                  <Text style={styles.planetData}>
+                    {details.specifications ? `Especificações: ` : ""}
+                  </Text>
+                  {details.specifications.map((item, index) => (
+                    <Text key={index.toString()} style={styles.planetData}>
+                      {item}
+                    </Text>
+                  ))}
+                </View>
+              </View>
             </View>
           </ImageBackground>
         </View>
       );
-    } else {
-      return (
-        <ImageBackground
-          source={require("../assets/bg.png")}
-          style={{ flex: 1, paddingTop: 20 }}
-        >
-          <View
-            style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
-          >
-            <Text style={styles.headerText}>Carregando...</Text>
-          </View>
-        </ImageBackground>
-      );
     }
+    return null;
   }
 }
